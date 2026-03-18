379501b14bfe558af3c67873662114edba941cd2
Ilay Ibrahimzadeh <ilay@bcmassets.com>
2026-03-18 19:24:01 +0200

    Switch to Central Portal publish endpoint

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,8 +67,8 @@ publishing {
     }
     repositories {
         maven {
-            name = "OSSRH"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            name = "CentralPortal"
+            url = uri("https://central.sonatype.com/api/v1/publisher/deployments/download/")
             credentials {
                 username = findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME") ?: ""
                 password = findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD") ?: ""
